0f4148f17bc41f98e0def63c8d149adb860b1d16
PareshGupta <paresh@vinsol.com>
2017-07-14 12:31:31 +0530

    change branch for vinsol_spree_themes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :rails_env, fetch(:stage)
 set :deploy_to, '/var/www/vinsol_spree_themes'
-set :branch, 'f-deploy'
+set :branch, 'master'
 
 server '35.163.245.199', roles: %w( app web db ), user: 'deploy'
